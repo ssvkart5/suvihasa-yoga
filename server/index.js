d602e1ed4ce61b7a6ea988a1f43b0a4e1bc401b0
ssvkart5 <ssvkart5@gmail.com>
2025-08-25 21:39:55 +0530

    updated docker files

--- server/index.js
+++ server/index.js
@@ -29,5 +29,5 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
