b6004f3aea4c5e1999f416911145cbe68cac4315
ssvkart5 <ssvkart5@gmail.com>
2025-09-04 00:20:05 +0530

    environments added

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.listen(port, () => {
 
 
 // MongoDB connection
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
